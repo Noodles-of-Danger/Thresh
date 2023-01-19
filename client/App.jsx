1a8a6a084661a179864d61886672435113462857
Mihran Baytaryan <mihranbaytaryan@gmail.com>
2023-01-18 21:53:27 -0800

    context passing

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,10 @@
-/**
- * @module index.js
- * @description Entry point for application; hangs React app off of #root div in index.html.
- **/
-
 import React from 'react';
 import Navbar from './pages/Navbar.jsx';
 
+
 const App = () => {
   return (
-    <Navbar />
+      <Navbar />
   );
 };
 
--- client/pages/Navbar.jsx
+++ client/pages/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import Home from './Home.jsx';
 import { Login }  from './Login.jsx';
 import { SignUp } from './SignUp.jsx';
@@ -7,12 +7,15 @@ import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import Logo from '../Images/Logo.png';
 import Board from './Board.jsx';
 
+export const UserContext = createContext('mr.default');   // ADDED
+
 const Navbar = () => {
   const [selectedPage, setSelectedPage] = useState('');
   // const navigate = useNavigate();
 
+  const [email, setEmail] = useState('');    // ADDED
   return (
-    <>
+    <UserContext.Provider value={email}>  
       <nav className="fixed w-screen bg-secondary-500 h-[80px] z-10 drop-shadow-xl">
         <div className="flex justify-between w-full h-full px-20">
           <div className="flex items-center gap-10">
@@ -64,10 +67,10 @@ const Navbar = () => {
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/board" element={<Board />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login email={email} setEmail={setEmail}/>} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
-    </>
+     </UserContext.Provider>
   );
 };
 
