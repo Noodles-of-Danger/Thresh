3ae0372c66613118a62c98ee35fcdb3c7a0153dc
KingzandBean <dbeame291@gmail.com>
2023-01-18 17:36:08 -0800

    added board route & getallboards & createboard endpoints

---
+++ server/routes/boardRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const boardController = require('../controllers/boardController');
+
+const router = express.Router();
+
+//GET ALL TASKS
+// Verify board cookie would be the middleware for this request once function previous function is implemented
+//
+router.get('/getBoards/:id', boardController.getAllBoards, (req, res, next) => {
+   res.status(200).json(res.locals.allBoards);
+});
+
+router.post('/createBoard', boardController.createBoard, boardController.createBoardCookie, (req, res, next) => {
+   res.status(200).json(res.locals.boardID);
+});
+
+module.exports = router;
